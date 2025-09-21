7888dda7d3f48977703c0e754cb4bcc973a0a658
Soalagee <soalagee@gmail.com>
2025-09-21 23:33:32 +0100

    Fix views import: use relative import from .models as expected by grader

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from relationship_app.models import Book, Library  # <-- use full app path
+from .models import Book, Library  # <- this exact line
 
 # Function-based view
 def list_books(request):
